ce863e1a2852edc93fab6c593f6f606e4752c421
Steven Bister <s.bister@bham.ac.uk>
2020-04-16 15:39:41 +0100

    Updates

---
+++ precache-manifest.f8286975dad81309fbe5fd13b3d3a457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4d7732f19617a4be2efb46f82dbc5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d83d149e6edbcb5307d",
+    "url": "/static/css/main.ee8c4ec3.chunk.css"
+  },
+  {
+    "revision": "5f9eed6224c406028733",
+    "url": "/static/js/2.fa581a99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fa581a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d83d149e6edbcb5307d",
+    "url": "/static/js/main.9afd2c11.chunk.js"
+  },
+  {
+    "revision": "f408fc3a1ca9d5954e67",
+    "url": "/static/js/runtime-main.30c6ae0f.js"
+  }
+]);
